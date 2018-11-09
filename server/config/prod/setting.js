c269907ded135cf79c3276ea78360e7fd867b887
beanson <2920248385@qq.com>
2018-11-09 15:43:42 +0800

    baseline

--- server/config/prod/setting.js
+++ server/config/prod/setting.js
@@ -3,18 +3,18 @@
  */
 //全局数据配置
 var settingData = {
-    basePath: "G:/SoftwareOutSourcing/https/front/httpsFront", //基础路径
-    domainName: "http://liphin.com", //域名设置
+    basePath: "/root/https/front", //基础路径
+    domainName: "https://liphin.com", //域名设置
     isProd: false, //是否为生产环境
     frontPort: 80 //前端port
 };
 //其他配置
-settingData['projectPath'] = settingData['basePath'] + "/project";
+settingData['projectPath'] = settingData['basePath'] + "/httpsFront";
 
 //https服务开启
 var serverConfig = {
-    key: 'G:/SoftwareOutSourcing/https/ca/Nginx/cert-1541484604580_liphin.com.key',
-    cert:'G:/SoftwareOutSourcing/https/ca/Nginx/cert-1541484604580_liphin.com.crt',
+    key: '/root/ca/https/node/cert-1541484604580_liphin.com.key',
+    cert:'/root/ca/https/node/cert-1541484604580_liphin.com.crt',
 };
 
 module.exports = {
