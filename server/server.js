7b163a5fdd996ca28e0d2031d61758b5e3a85c5e
beanson <2920248385@qq.com>
2018-11-28 13:09:14 +0800

    baseline

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ if (env == undefined || env == '' || env == null) {
 var fs = require('fs');
 var http = require('http');
 var https = require('https');
+var util = require('util');
 var request = require('request');
 var multer = require('multer');
 var express = require('express');
@@ -34,6 +35,33 @@ app.use(bodyParser.urlencoded({limit: serverSerData.httpDataLimit, extended: tru
 // app.use(function (req, res, next) {
 //     serHelper.setCrossOrigin(req, res, next);
 // });
+
+
+app.get('/getWxUserInfo', function (req, res) {
+    var appid = 'wx16c7efa55a7f976b';
+    var secret = '6e4698fa5da26b2639e9a578557ddc55';
+    var arg = url.parse(req.url, true).query;
+    var code = arg['code'];
+    var getAccessTokenUrl = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=%s&secret=%s&code=%s&grant_type=authorization_code";
+
+    var uri = util.format(getAccessTokenUrl, appid, secret,code);
+    //用request方式请求
+    request.get(uri, function (err, resData, body) {
+        if (!err && resData['statusCode'] == 200) {
+            var userinfo= JSON.parse(body);
+            console.log(userinfo);
+            res.send(userinfo);
+
+        } else {
+            //调用出错
+            console.error("getAccessToken error: ", err);
+            if (res != null) {
+                res.send('error');
+            }
+        }
+    });
+});
+
 
 /**
  * 返回基础配置文件
